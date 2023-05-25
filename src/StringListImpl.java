edeb76b9a5c89ee1010f6c939f12d7f05e72d696
Александр <hyperpositron@gmail.com>
2023-05-25 17:51:01 +0200

    done

--- src/StringListImpl.java
+++ src/StringListImpl.java
@@ -15,52 +15,92 @@ public class StringListImpl implements StringList {
 
     @Override
     public String add(String item) {
-        return null;
+        validateSize();
+        validateItem(item);
+        storage[size++] = item;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-        return null;
+        validateSize();
+        validateItem(item);
+        validateIndex(index);
+        if (index == size) {
+            storage[size++] = item;
+            return item;
+        }
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
+        size++;
+        return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        validateIndex(index);
+        validateItem(item);
+        storage[index] = item;
+        return item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        validateItem(item);
+
+        int index = indexOf(item);
+
+        return remove(index);
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        validateIndex(index);
+
+        String item = storage[index];
+
+        if (index != size) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        }
+
+        size--;
+        return item;
     }
 
     @Override
     public boolean contains(String item) {
-        return false;
+        return indexOf(item) != -1;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        validateIndex(index);
+        return storage[index];
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return Arrays.equals(this.toArray(),otherList.toArray());
+        return Arrays.equals(this.toArray(), otherList.toArray());
     }
 
     @Override
